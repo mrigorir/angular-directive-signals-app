0c9ac3ebe53296c8cc2b654c470814eac0009490
Marco Parra <marcoparra311@gmail.com>
2024-07-30 12:20:36 -0400

    Set errors on screen depending on validations through customLabel directive

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,10 +7,16 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color:string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value:string) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
   }
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -23,6 +30,28 @@ export class CustomLabelDirective implements OnInit {
   setStyle():void {
     if (!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage():void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = 'No hay errores';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+    if (errors.includes('minlength')) {
+      this.htmlElement.nativeElement.innerText = `Requiere almenos ${this._errors['minlength']} dígitos van ${this._errors['actualLength']} `;
+      return;
+    }
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText = 'Formato de correo inválido';
+      return;
+    }
   }
 
 }
